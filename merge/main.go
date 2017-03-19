69a5c50fb88eac255fe2efba3adb45617fc2374e
armiller <anthonymiller@outlook.com>
2017-03-18 23:23:45 -0700

    Use while loop and just append left over

--- merge/main.go
+++ merge/main.go
@@ -5,32 +5,29 @@ import (
 )
 
 func main() {
-	a := []int{3}
-	b := []int{2, 3, 4}
+	a := []int{3, 5, 7}
+	b := []int{3, 3, 4}
 	c := merge(a, b)
 	fmt.Println(c)
 }
 
 func merge(a, b []int) []int {
 	var combined []int
-	total := len(a) + len(b)
 	x, y := 0, 0
-	for i := 0; i < total; i++ {
-		if x < len(a) && y < len(b) {
-			if a[x] < b[y] {
-				combined = append(combined, a[x])
-				x++
-			} else {
-				combined = append(combined, b[y])
-				y++
-			}
-		} else if x < len(a) {
+	for x < len(a) && y < len(b) {
+		if a[x] < b[y] {
 			combined = append(combined, a[x])
 			x++
 		} else {
 			combined = append(combined, b[y])
 			y++
 		}
+	}
+	if len(a)-x != 0 {
+		combined = append(combined, a[x:]...)
+	}
+	if len(b)-y != 0 {
+		combined = append(combined, b[y:]...)
 	}
 	return combined
 }
